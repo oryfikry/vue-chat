688521f205d57ead65629de25056b6a608ba92fb
oryfikry <oryza4444@gmail.com>
2021-04-18 08:53:04 +0700

    add

--- src/db.js
+++ src/db.js
@@ -2,13 +2,8 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const  config = {
-    // API KEY
-    apiKey: "AIzaSyCy5MKIhbq-punfLumuU3MuqSRLkuefn3c",
-    authDomain: "vue-chat-6e6fd.firebaseapp.com",
-    projectId: "vue-chat-6e6fd",
-    storageBucket: "vue-chat-6e6fd.appspot.com",
-    messagingSenderId: "63015244740",
-    appId: "1:63015244740:web:1c38350d10b274c7c10a91"
+    // Firebase API KEY
+   
 } 
 
 const db = firebase.initializeApp(config);
